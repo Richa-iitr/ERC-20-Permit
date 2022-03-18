f22c1b60f89b54c1dbe269e3141cde190b331f6e
Richa-iitr <richa@cs.iitr.ac.in>
2022-03-19 04:18:53 +0530

    updated tests

--- test/daiPermit.js
+++ test/daiPermit.js
@@ -6,35 +6,52 @@ const {ecsign, ecrecover, pubToAddress} = require("ethereumjs-util");
 
 describe("DAI deposit", function () {
 
-  let signer, daideposit, expiry;
+  let signer, daideposit, expiry, dai;
   const PERMIT_TYPEHASH = '0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb';
   const DOMAIN_SEPARATOR = '0xdbb8cf42e1ecb028be3f3dbc922e1d878b963f411dc388ced501601c60f7c6f7';
   const signerAddr = "0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf";
   const holder = "0x15C6b352c1F767Fa2d79625a40Ca4087Fab9a198";
   const privateKey = process.env.PRIVATE_KEY;    //private key of the owner
+  const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const daiAbi = [{"inputs":[{"internalType":"uint256","name":"chainId_","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"src","type":"address"},{"indexed":true,"internalType":"address","name":"guy","type":"address"},{"indexed":false,"internalType":"uint256","name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":true,"inputs":[{"indexed":true,"internalType":"bytes4","name":"sig","type":"bytes4"},{"indexed":true,"internalType":"address","name":"usr","type":"address"},{"indexed":true,"internalType":"bytes32","name":"arg1","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"arg2","type":"bytes32"},{"indexed":false,"internalType":"bytes","name":"data","type":"bytes"}],"name":"LogNote","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"src","type":"address"},{"indexed":true,"internalType":"address","name":"dst","type":"address"},{"indexed":false,"internalType":"uint256","name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"guy","type":"address"}],"name":"deny","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"move","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"holder","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"bool","name":"allowed","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"pull","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"push","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"guy","type":"address"}],"name":"rely","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"wards","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+  const signeAddr = "0x1B7BAa734C00298b9429b518D621753Bb0f6efF2";
 
   beforeEach(async function () {
+    let [addr1,addr2, addr3, addr4, addr5] = await ethers.getSigners();
     const DaiDeposit = await ethers.getContractFactory("DaiDeposit");
     daideposit = await DaiDeposit.deploy();
     await daideposit.deployed();
-    console.log(daideposit.address);
+    // console.log(`Address: ${daideposit.address}`);
 
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: [signerAddr],
+      params: [signeAddr],
     });
-    signer = await ethers.getSigner(signerAddr);
+    signer = await ethers.getSigner(signeAddr);
     await hre.network.provider.send("hardhat_setBalance", [
       holder,
       "0x9F4F2726179A224501D762422C946590D9100",
     ]);
+    dai = await new ethers.Contract(daiAddress, daiAbi);
+    console.log(await dai.connect(signer).balanceOf(signer.address));
+    await dai.connect(signer).transfer(holder, 1);
+    // console.log(await dai.connect(signer).balanceOf(holder));
   });
+
 
   it("Should return the transaction hash on successful transfer", async function () {
 
+    
+    let sBalance = await dai.connect(signer).balanceOf(daideposit.address);
+    console.log(`Balance before transaction: ${sBalance}`);
+    console.log();
+
     expiry = Date.now() + 20*60;
-    let amount = 0;
-    // let nonce = daideposit.getNonce(holder);         
+    let amount = 1;
+    console.log(`Amount: ${amount}` );
+    console.log();
+    // let nonce = daideposit.getNonce(holder);   
+
     let nonce=0;
     const digest = ethers.utils.keccak256(
       ethers.utils.solidityPack(
@@ -54,10 +71,14 @@ describe("DAI deposit", function () {
     );
 
     const { v, r, s } = ecsign(Buffer.from(digest.slice(2), 'hex'), Buffer.from(privateKey,'hex'));  
-    const addr = ecrecover(Buffer.from(digest.slice(2),'hex'), v,r,s);
-    console.log(pubToAddress(addr));  
+    // const addr = ecrecover(Buffer.from(digest.slice(2),'hex'), v,r,s);
+    // console.log(pubToAddress(addr));  
     let txHash = await daideposit.connect(signer).deposit(holder, amount, v, r, s, expiry, nonce);
     console.log("Transaction hash: ");
     console.log(txHash);
+    console.log();
+
+    let eBalance = await dai.connect(signer).balanceOf(daideposit.address);
+    console.log(`Balance after transaction: ${eBalance}`);
   });
 });
